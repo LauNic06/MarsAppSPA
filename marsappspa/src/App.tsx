505381000bd7716dccc26bc21ba2b2f612db8edc
Laura Nicoara <Laura.Nicoara@softwire.com>
2022-07-01 13:21:16 +0100

    Done Part3.1 - Components in components

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -10,6 +10,26 @@ interface TemplateProps {
     image: string;
 }
 
+interface Component1Props {
+    component2: Component2Props;
+    component3: Component3Props;
+}
+
+interface Component2Props {
+    count: number;
+    setCount: (value: (((prevState: number) => number) | number)) => void;
+}
+
+interface Component3Props {
+    component4: Component4Props;
+    message: string;
+}
+
+interface Component4Props {
+    count: number;
+}
+
+
 function Template(props: TemplateProps) {
   return (
       <p>
@@ -21,11 +41,45 @@ function Template(props: TemplateProps) {
   );
 }
 
+function Component1(props: Component1Props) {
+    return (
+        <div>
+            <Component3 component4={{count: props.component3.component4.count}} message={"Component3's message"}/>
+            <Component2 count={props.component2.count} setCount={props.component2.setCount} />
+        </div>
+    );
+}
+
+function Component2(props: Component2Props) {
+    return (
+        <button onClick={() => props.setCount(props.count + 1)}>
+            Click here
+        </button>
+    );
+}
+
+function Component3(props: Component3Props) {
+    return(
+        <div>
+            <Component4 count={props.component4.count} />
+            <p>{props.message}</p>
+        </div>
+    );
+}
+
+function Component4(props: Component4Props) {
+    return (
+        <p>You clicked {props.count} times</p>
+    );
+}
+
 function App() {
     let countTemp = localStorage.getItem('count') || '0';
 
     const [count, setCount] = useState(parseInt(countTemp));
 
+    // const component4Props: Component4Props = {count: 0};
+    
     useEffect(() => {
         document.title = `You clicked ${count} times`;
         window.localStorage.setItem('count', count.toString());
@@ -36,10 +90,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Template title="NASA" p1="Astronomy Picture of the Day" p2="2001 March 6" image={dumbbell}/>
-          <p>You clicked {count} times</p>
-          <button onClick={() => setCount(count + 1)}>
-              Click here
-          </button>
+
+          <Component1 component2={{count: count, setCount: setCount}} component3={{component4: {count: count}, message: "Component3's message"}} />
+          {/*<Component3 component4={{count: count}} message={"Component3's message"}/>*/}
+          {/*<Component2 count={count} setCount={setCount} />*/}
         <a
           className="App-link"
           href="https://reactjs.org"
