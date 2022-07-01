b86b8877fd6ff73a2a1af0dc4791590eeb2effc0
Laura Nicoara <Laura.Nicoara@softwire.com>
2022-07-01 16:18:42 +0100

    Started Strech Goals - made three links

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import dumbbell from './dumbbell_noao.jpg';
 import './App.css';
+import {Link, Route, BrowserRouter as Router, Routes, useLocation} from "react-router-dom";
+// import {}
 
 interface TemplateProps {
     title: string;
@@ -64,6 +66,18 @@ function Component4() {
     );
 }
 
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function AppHeader() {
+    return <h2>AppHeader</h2>;
+}
+
+function AppLogo() {
+    return <img src={logo} className="App-logo" alt="logo" />;
+}
+
 let userDetailContext = React.createContext<Component2Props>(null as unknown as Component2Props);
 
 function App() {
@@ -79,25 +93,53 @@ function App() {
     }, [count])
 
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Template title="NASA" p1="Astronomy Picture of the Day" p2="2001 March 6" image={dumbbell}/>
-                <userDetailContext.Provider value={{count: count, setCount: setCount}}>
-                    <Component1 />
-                </userDetailContext.Provider>
-                {/*<Component3 component4={{count: count}} message={"Component3's message"}/>*/}
-                {/*<Component2 count={count} setCount={setCount} />*/}
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Router>
+            <div className="App">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/app-header">AppHeader</Link>
+                    </li>
+                    <li>
+                        <Link to="/app-logo">App-Logo</Link>
+                    </li>
+                    {/*<li>*/}
+                    {/*    <Link to="/details">Details</Link>*/}
+                    {/*</li>*/}
+                    {/*<li>*/}
+                    {/*    <Link to="/components">Components</Link>*/}
+                    {/*</li>*/}
+                </ul>
+
+
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/app-header" element={<AppHeader/>} />
+                    <Route path="/app-logo" element={<AppLogo/>} />
+                </Routes>
+            </div>
+            {/*<div className="App">*/}
+            {/*    <header className="App-header">*/}
+            {/*        <img src={logo} className="App-logo" alt="logo" />*/}
+            {/*        <Template title="NASA" p1="Astronomy Picture of the Day" p2="2001 March 6" image={dumbbell}/>*/}
+            {/*        <userDetailContext.Provider value={{count: count, setCount: setCount}}>*/}
+            {/*            <Component1 />*/}
+            {/*        </userDetailContext.Provider>*/}
+            {/*        /!*<Component3 component4={{count: count}} message={"Component3's message"}/>*!/*/}
+            {/*        /!*<Component2 count={count} setCount={setCount} />*!/*/}
+            {/*        <a*/}
+            {/*            className="App-link"*/}
+            {/*            href="https://reactjs.org"*/}
+            {/*            target="_blank"*/}
+            {/*            rel="noopener noreferrer"*/}
+            {/*        >*/}
+            {/*            Learn React*/}
+            {/*        </a>*/}
+            {/*    </header>*/}
+            {/*</div>*/}
+        </Router>
     );
 }
 
