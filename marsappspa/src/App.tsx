8f44d6b7ed08d5b438220e249f5ae1372744a2e0
Laura Nicoara <Laura.Nicoara@softwire.com>
2022-07-04 08:13:40 +0100

    Done multiple pages with different routes

--- marsappspa/src/App.tsx
+++ marsappspa/src/App.tsx
@@ -105,40 +105,25 @@ function App() {
                     <li>
                         <Link to="/app-logo">App-Logo</Link>
                     </li>
-                    {/*<li>*/}
-                    {/*    <Link to="/details">Details</Link>*/}
-                    {/*</li>*/}
-                    {/*<li>*/}
-                    {/*    <Link to="/components">Components</Link>*/}
-                    {/*</li>*/}
+                    <li>
+                        <Link to="/details">Details</Link>
+                    </li>
+                    <li>
+                        <Link to="/components">Components</Link>
+                    </li>
                 </ul>
-
 
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/app-header" element={<AppHeader/>} />
                     <Route path="/app-logo" element={<AppLogo/>} />
+                    <Route path="/details" element={<Template title="NASA" p1="Astronomy Picture of the Day" p2="2001 March 6" image={dumbbell}/>} />
+                    <Route
+                        path="/components"
+                        element={ <userDetailContext.Provider value={{count: count, setCount: setCount}}> <Component1 />
+                                </userDetailContext.Provider>} />
                 </Routes>
             </div>
-            {/*<div className="App">*/}
-            {/*    <header className="App-header">*/}
-            {/*        <img src={logo} className="App-logo" alt="logo" />*/}
-            {/*        <Template title="NASA" p1="Astronomy Picture of the Day" p2="2001 March 6" image={dumbbell}/>*/}
-            {/*        <userDetailContext.Provider value={{count: count, setCount: setCount}}>*/}
-            {/*            <Component1 />*/}
-            {/*        </userDetailContext.Provider>*/}
-            {/*        /!*<Component3 component4={{count: count}} message={"Component3's message"}/>*!/*/}
-            {/*        /!*<Component2 count={count} setCount={setCount} />*!/*/}
-            {/*        <a*/}
-            {/*            className="App-link"*/}
-            {/*            href="https://reactjs.org"*/}
-            {/*            target="_blank"*/}
-            {/*            rel="noopener noreferrer"*/}
-            {/*        >*/}
-            {/*            Learn React*/}
-            {/*        </a>*/}
-            {/*    </header>*/}
-            {/*</div>*/}
         </Router>
     );
 }
